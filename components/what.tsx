63d4cb35b6d5cf9f0f1f3bf156c4e8aa0ec201f6
Dela <ernestanyomitse752@gmail.com>
2023-06-06 01:22:33 +0000

    Navbar

--- components/what.tsx
+++ components/what.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function what() {
-  return <div>what</div>;
+  return (
+    <div>
+      <div>
+        <h1>What We Offer</h1>{" "}
+      </div>
+    </div>
+  );
 }
 
 export default what;
