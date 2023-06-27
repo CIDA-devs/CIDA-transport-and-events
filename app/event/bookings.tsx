b404ee6527d8fd2aa00945124f65adb431a1ca1e
Winifred <winifredasante15@gmail.com>
2023-06-27 01:07:44 -0400

    added more styles

---
+++ app/event/bookings.tsx
@@ -0,0 +1,68 @@
+import hall from "../../components/images/hall.jpg";
+import hall2 from "../../components/images/hall2.jpg";
+import birthday from "../../components/images/birthday.jpg";
+import seminar from "../../components/images/seminar.jpg";
+import conference from "../../components/images/conference.jpg";
+
+export const bookings = [
+    {
+      id: 1,
+      img: hall,
+      hallName: "Red House Hall",
+      streetName: "Street Name",
+      phoneNumber: "0000 000 000",
+      bigHallPrice: "50,000",
+      lightPrice: "10,000",
+      flowerPrice: "2,000",
+      decor: "20,000",
+      category: "Wedding",
+    },
+    {
+      id: 2,
+      img: hall2,
+      hallName: "WeOutSide Hall",
+      streetName: "Street Name",
+      phoneNumber: "0000 000 000",
+      bigHallPrice: "70,000",
+      lightPrice: "0",
+      flowerPrice: "5,000",
+      decor: "30,000",
+      category: "Wedding",
+    },
+    {
+      id: 3,
+      img: birthday,
+      hallName: "Sprinter Pool",
+      streetName: "Street Name",
+      phoneNumber: "0000 000 000",
+      bigHallPrice: "50,000",
+      lightPrice: "10,000",
+      flowerPrice: "2,000",
+      decor: "20,000",
+      category: "Birthday",
+    },
+    {
+      id: 4,
+      img: seminar,
+      hallName: "Trojan Horse Hall",
+      streetName: "Street Name",
+      phoneNumber: "0000 000 000",
+      bigHallPrice: "70,000",
+      lightPrice: "0",
+      flowerPrice: "5,000",
+      decor: "5,000",
+      category: "Seminar",
+    },
+    {
+      id: 5,
+      img: conference,
+      hallName: "Starlight Conference Hall",
+      streetName: "Street Name",
+      phoneNumber: "0000 000 000",
+      bigHallPrice: "70,000",
+      lightPrice: "0",
+      flowerPrice: "5,000",
+      decor: "20,000",
+      category: "Conference",
+    },
+  ];
--- app/event/page.tsx
+++ app/event/page.tsx
@@ -9,77 +9,12 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { FaMap, FaMapMarker, FaPhone, FaSearch } from "react-icons/fa";
 
-import hall from "../../components/images/hall.jpg";
-import hall2 from "../../components/images/hall2.jpg";
-import birthday from "../../components/images/birthday.jpg";
-import seminar from "../../components/images/seminar.jpg";
-import conference from "../../components/images/conference.jpg";
+import { bookings } from "./bookings";
 import next from "next/types";
 
 const About = () => {
   const [search, setSearch] = useState("");
-  const bookings = [
-    {
-      id: 1,
-      img: hall,
-      hallName: "Red House Hall",
-      streetName: "Street Name",
-      phoneNumber: "0000 000 000",
-      bigHallPrice: "50,000",
-      lightPrice: "10,000",
-      flowerPrice: "2,000",
-      decor: "20,000",
-      category: "Wedding",
-    },
-    {
-      id: 2,
-      img: hall2,
-      hallName: "WeOutSide Hall",
-      streetName: "Street Name",
-      phoneNumber: "0000 000 000",
-      bigHallPrice: "70,000",
-      lightPrice: "0",
-      flowerPrice: "5,000",
-      decor: "30,000",
-      category: "Wedding",
-    },
-    {
-      id: 3,
-      img: birthday,
-      hallName: "Sprinter Pool",
-      streetName: "Street Name",
-      phoneNumber: "0000 000 000",
-      bigHallPrice: "50,000",
-      lightPrice: "10,000",
-      flowerPrice: "2,000",
-      decor: "20,000",
-      category: "Birthday",
-    },
-    {
-      id: 4,
-      img: seminar,
-      hallName: "Trojan Horse Hall",
-      streetName: "Street Name",
-      phoneNumber: "0000 000 000",
-      bigHallPrice: "70,000",
-      lightPrice: "0",
-      flowerPrice: "5,000",
-      decor: "5,000",
-      category: "Seminar",
-    },
-    {
-      id: 5,
-      img: conference,
-      hallName: "Starlight Conference Hall",
-      streetName: "Street Name",
-      phoneNumber: "0000 000 000",
-      bigHallPrice: "70,000",
-      lightPrice: "0",
-      flowerPrice: "5,000",
-      decor: "20,000",
-      category: "Conference",
-    },
-  ];
+ 
 
   return (
     <>
